93e3eac76da81ae5ed7115cfaebb53cea0e46c4a
coder <tsering@Tsering>
2022-11-29 17:08:03 +0545

    ViewSet Urls Router Added in Urls

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 # from .views import *
 from profiles_api import views
 
+#? View Set URLs Router 
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
+# ? Regular Urls
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view()), 
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls)),
 ]
