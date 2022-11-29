c9c4496b37d21ee55d5f6936a677d490d5c8f1a1
coder <tsering@Tsering>
2022-11-29 07:53:25 +0545

    URLs added to Profiles_API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,5 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.views import APIView #API VIEW 
+from rest_framework.response import Response #For Response
 
 
 class HelloApiView(APIView):
