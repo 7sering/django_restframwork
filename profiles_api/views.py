6ce2c9edf9eb74a552e417207facf936ada843fa
coder <tsering@Tsering>
2022-11-29 10:15:11 +0545

    Serializer file & Post added to API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,24 @@
-from rest_framework.views import APIView #API VIEW 
-from rest_framework.response import Response #For Response
+from rest_framework.views import APIView  # API VIEW
+from rest_framework.response import Response  # For Response
+
+from rest_framework import status
+from profiles_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+           return YOUR_SERIALIZER_1
+        elif self.request.method == 'GET':
+           return YOUR_SERIALIZER_2
+        else:
+            return YOUR_DEFAULT_SERIALIZER
+
+
 
     def get(self, request, format=None):
         """Retrun a list of API View Features"""
@@ -15,3 +30,19 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+    
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
