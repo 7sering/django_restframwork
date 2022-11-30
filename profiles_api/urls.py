53ef8506361e6750b0fa105fd9df59d2aca87225
coder <tsering@Tsering>
2022-11-30 13:10:52 +0545

    Modal ViewSet URL added in URLs.py

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,6 +7,9 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 
+#Modal View Set no need to call base_name
+router.register('profile', views.UserProfileViewSet())
+
 # ? Regular Urls
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
