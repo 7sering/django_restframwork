1559608df3b098d0da9101df618c28ef2854f564
coder <tsering@Tsering>
2022-12-06 20:09:00 +0545

    Error fixed and Profile  Feed  model migrated

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -64,7 +64,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 class ProfileFeedItem(models.Model):
     """ Profile Status Update """
     user_profile = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=CASCADE)
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     status_text = models.CharField(max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
 
